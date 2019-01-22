a2cd94b9a8479e3238febca4f89657387ae1bb58
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2019-01-22 14:48:22 +0100

    Creato array multidimensionale delle partite

--- esercizioDue.php
+++ esercizioDue.php
@@ -6,5 +6,35 @@
   </head>
   <body>
 
+    <?php
+      /*
+      creo un array contenente le partite di basket di un'ipotetica tappa del
+      calendario. Ogni array avrà una squadra di casa e una squadra ospite.
+      Avremo poi sempre per ogni array i punti fatti dalla squadra di casa e i
+      punti fatti dalla squadra in trasferta.
+      */
+      $partite = [
+        [
+          'squadraCasa' => 'Virtus',
+          'squadraOspite' => 'Ferrara kleb'.
+          'puntiCasa' => 107,
+          'puntiOspite' => 109,
+        ],
+        [
+          'squadraCasa' => 'Armani milano',
+          'squadraOspite' => 'New basket brindisi'.
+          'puntiCasa' => 97,
+          'puntiOspite' => 102,
+        ],
+        [
+          'squadraCasa' => 'Acquila basket trento',
+          'squadraOspite' => 'Polisportiva dinamo'.
+          'puntiCasa' => 87,
+          'puntiOspite' => 83,
+        ],
+      ] ;
+
+     ?>
+
   </body>
 </html>
